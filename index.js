c52c0b16c2c8ce5ed4bd31324d6fe601c7a80bc3
Sunny Junior <sunnepazzy123@gmail.com>
2020-07-07 05:55:27 +0100

    new variable added heroku

--- index.js
+++ index.js
@@ -21,7 +21,12 @@ const passport = require("passport");
 //DATABASE CONNECTION
 // const mongoDbUrl = "mongodb://localhost:27017/bankapp" ;
 
-const mongoDbUrl = "mongodb://bankapp123:bankapp123@cluster0-shard-00-00-ul9hr.mongodb.net:27017,cluster0-shard-00-01-ul9hr.mongodb.net:27017,cluster0-shard-00-02-ul9hr.mongodb.net:27017/<dbname>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+    const  mongoDbUrl = process.env.MONGO_DB_URI
+    
+
+
+
+// const mongoDbUrl = "mongodb://bankapp123:bankapp123@cluster0-shard-00-00-ul9hr.mongodb.net:27017,cluster0-shard-00-01-ul9hr.mongodb.net:27017,cluster0-shard-00-02-ul9hr.mongodb.net:27017/<dbname>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 mongoose.connect(mongoDbUrl, {useUnifiedTopology: true, useNewUrlParser: true } )
         .then(db=>{
